5126d988c0c0ecdc37a81d4bd7c1d950ff4275f2
Nitin Solanki <nitin.photos1210@gmail.com>
2025-05-06 09:31:24 +0000

    Newr

--- models/Source/product_master.sql
+++ models/Source/product_master.sql
@@ -1,6 +1,10 @@
 -- use warehouse compute_wh;
-select t.*, c.CATEGORYNAME,sc.SUBCATEGORYNAME, c.PRODUCTCATEGORYKEY 
+with data1 as
+(select t.*, c.CATEGORYNAME,sc.SUBCATEGORYNAME, c.PRODUCTCATEGORYKEY 
 from adv_workspace.dashboard.product t
 left join adv_workspace.dashboard.sub_category as sc on sc.PRODUCTSUBCATEGORYKEY = t.PRODUCTSUBCATEGORYKEY
-left join adv_workspace.dashboard.category as c on c.PRODUCTCATEGORYKEY = sc.PRODUCTCATEGORYKEY
+left join adv_workspace.dashboard.category as c on c.PRODUCTCATEGORYKEY = sc.PRODUCTCATEGORYKEY)
+
+select * from data1
+
 
