1779e9eb4f9c7d847fd28da48e4ec2701d01b44e
Nitin Solanki <nitin.photos1210@gmail.com>
2025-05-06 08:24:01 +0000

    Second

--- models/Source/product_master.sql
+++ models/Source/product_master.sql
@@ -4,4 +4,3 @@ from adv_workspace.dashboard.product t
 left join adv_workspace.dashboard.sub_category as sc on sc.PRODUCTSUBCATEGORYKEY = t.PRODUCTSUBCATEGORYKEY
 left join adv_workspace.dashboard.category as c on c.PRODUCTCATEGORYKEY = sc.PRODUCTCATEGORYKEY
 
-
